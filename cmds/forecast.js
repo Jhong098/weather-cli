ffae66064047d049ad40f93c74fc3c55f9913e10
Jhong098 <Jhong098@gmail.com>
2018-06-21 23:44:31 -0400

    get weather based on location api call

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -1,3 +1,23 @@
-module.exports = (args) => {
-    console.log('tomorrow is rainy')
+const ora = require('ora')
+const getWeather = require('../utils/weather')
+
+module.exports = async (args) => {
+    const spinner = ora().start()
+
+    try {
+        const location = args.location || args.l
+        const weather = await getWeather(location)
+
+        spinner.stop()
+
+        console.log(`Forecast in ${location}:`)
+        weather.forecast.forEach(item =>
+            console.log(`\t${item.date} - Low: ${item.low}° | High: ${item.high}° | ${item.text}`)
+        )
+    } catch (err) {
+        spinner.stop()
+
+        console.error(err);
+        
+    }
 }
